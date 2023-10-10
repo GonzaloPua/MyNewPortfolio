134805d731d7879be832da88e43645ad9fbb568d
Nicolas <nicopua7@gmail.com>
2023-10-09 22:52:59 -0300

    information about me created

---
+++ myPortfolio/src/projects/userData.ts
@@ -0,0 +1,177 @@
+export default {
+    projects: [
+        {
+            name: "My Portfolio",
+            description: "Portfolio personal en dónde se encuentra ubicada toda la información acerca de mí y de mis proyectos. También se puede encontrar las tecnologías con las que estoy trabajando actualmente, podrás descargar mi CV y ponerte en contacto conmigo.",
+            participants: "Autónomo",
+            duration: "1 semana",
+            links: {
+                github: "https://github.com/NicoPua/MyPortfolio"
+            },
+            technologies:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868383/personal_folder/techLogos/nexticon_ayxbm8.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/react_d521px.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607553/personal_folder/techLogos/typescript_syou3b.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868065/personal_folder/techLogos/nodejs_vtklge.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/sass_q9bxan.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/cloudinary_vzdxg5.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607546/personal_folder/techLogos/nodemailer_f2rqxc.png"     
+            ],
+            images:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696551329/personal_folder/homePicsArt_d6xnwn.jpg"
+            ]
+        },{
+            name: "PicsArt Gallery",
+            description: "sadsda",
+            participants: "Autónomo",
+            duration: "07/2023 - 09/2023 (3 meses)",
+            links: {
+                deploy: "https://picsart-gallery.vercel.app/",
+                github: "https://github.com/NicoPua/ImagesGalery"
+            },
+            technologies:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868383/personal_folder/techLogos/nexticon_ayxbm8.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/react_d521px.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607553/personal_folder/techLogos/typescript_syou3b.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868065/personal_folder/techLogos/nodejs_vtklge.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866690/personal_folder/techLogos/tailwind_lo3q6z.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607551/personal_folder/techLogos/redux_jlwtei.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696608333/personal_folder/techLogos/mongodb_p8ozsf.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/descarga_vtktow.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/cloudinary_vzdxg5.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607546/personal_folder/techLogos/nodemailer_f2rqxc.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607546/personal_folder/techLogos/nextauth_ht1dwy.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607553/personal_folder/techLogos/recaptcha_hryxei.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/git_pzgqbx.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/github_k4cmtc.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/Axios_aulbag.png"
+            ],
+            images:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696551329/personal_folder/homePicsArt_d6xnwn.jpg"
+            ]
+        },{
+            name: "Finder Service",
+            description: "asdasd",
+            participants: "Grupal (6 integrantes)",
+            duration: "05/2023 - 06/2023 (2 meses)",
+            links: {
+                youtube: "https://www.youtube.com/watch?v=EicCl16HXMM",
+                deploy: "https://finder-service-bice.vercel.app/",
+                github: "https://github.com/NicoPua/FinderService"
+            },
+            technologies:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868383/personal_folder/techLogos/nexticon_ayxbm8.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/react_d521px.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866690/personal_folder/techLogos/tailwind_lo3q6z.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868065/personal_folder/techLogos/nodejs_vtklge.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/javascript_mikep8.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696608333/personal_folder/techLogos/mongodb_p8ozsf.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/descarga_vtktow.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/cloudinary_vzdxg5.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/anotherMailgun_mrvki3.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607546/personal_folder/techLogos/nextauth_ht1dwy.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/git_pzgqbx.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/github_k4cmtc.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/Axios_aulbag.png"
+            ],
+            images:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696552234/personal_folder/FinderserviceHome_az3d0b.jpg",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696552234/personal_folder/FinderLogin_seuwoj.jpg",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696552153/personal_folder/FinderRegister_wyhamo.jpg",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696552153/personal_folder/HomeEmployer_mplrka.jpg"
+            ]
+        },{
+            name: "Videogames APP",
+            description: "Esta Single Page Application (SPA) ha sido diseñada y desarrollada con el fin de poder visualizar información detallada de videojuegos utlizando una API externa la cual brinda información de +1000 videojuegos. El usuario es capaz de realizar una búsqueda sobre su videojuego favorito y encontrar información adicional al respecto, también es capaz de añadir o crear una card del videojuego que desee.",
+            participants: "Autónomo",
+            duration: "04/2023 - 04/2023 (1 mes)",
+            links: {
+                github: "https://github.com/NicoPua/Proyecto-VideoGames"
+            },
+            technologies:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/react_d521px.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868065/personal_folder/techLogos/nodejs_vtklge.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/javascript_mikep8.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/express_wdxp1m.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607550/personal_folder/techLogos/postgresql_e6shrm.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607549/personal_folder/techLogos/sql_b7fxgo.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607551/personal_folder/techLogos/sequelize_c9vfio.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/csslogo_fbyfym.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/htmllogo_y14k0b.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/git_pzgqbx.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/github_k4cmtc.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/Axios_aulbag.png"
+            ],
+            images:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696551299/personal_folder/homeVideo_l6oemn.jpg", 
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696551299/personal_folder/landingVideo_ep96zx.jpg",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696551299/personal_folder/postVideo_wxkm41.jpg"
+            ]
+        },{
+            name: "Rick and Morty APP",
+            description: "Esta Single Page Application (SPA) ha sido diseñada y desarrollada con el fin de poder visualizar información detallada de videojuegos utlizando una API externa la cual brinda información de +1000 videojuegos. El usuario es capaz de realizar una búsqueda sobre su videojuego favorito y encontrar información adicional al respecto, también es capaz de añadir o crear una card del videojuego que desee.",
+            participants: "Autónomo",
+            duration: "02/2023 - 04/2023 (2 meses)",
+            links: {
+                github: "https://github.com/NicoPua/Proyecto-Rick-Morty"
+            },
+            technologies:[
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/react_d521px.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868065/personal_folder/techLogos/nodejs_vtklge.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/javascript_mikep8.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/express_wdxp1m.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/git_pzgqbx.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/github_k4cmtc.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/Axios_aulbag.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/csslogo_fbyfym.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607549/personal_folder/techLogos/styled-components_pvgrte.png",
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/htmllogo_y14k0b.png"
+            ],
+            images: [
+                "https://res.cloudinary.com/djngnnxvp/image/upload/v1692936028/cld-sample-3.jpg"
+            ]
+        }
+    ],
+    technologies: {
+        techs: [
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/javascript_mikep8.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607553/personal_folder/techLogos/typescript_syou3b.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868065/personal_folder/techLogos/nodejs_vtklge.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/react_d521px.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696868235/personal_folder/techLogos/next.js_nqsiqv.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607551/personal_folder/techLogos/redux_jlwtei.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607546/personal_folder/techLogos/nextauth_ht1dwy.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/express_wdxp1m.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/csslogo_fbyfym.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/htmllogo_y14k0b.png"
+        ],
+        desing: [
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866690/personal_folder/techLogos/tailwind_lo3q6z.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607548/personal_folder/techLogos/sass_q9bxan.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607549/personal_folder/techLogos/styled-components_pvgrte.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607554/personal_folder/techLogos/bootstrap_ul1kts.png"
+        ],
+        database: [
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607550/personal_folder/techLogos/postgresql_e6shrm.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607549/personal_folder/techLogos/sql_b7fxgo.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607551/personal_folder/techLogos/sequelize_c9vfio.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696608333/personal_folder/techLogos/mongodb_p8ozsf.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/descarga_vtktow.png"
+        ],
+        tools: [
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607552/personal_folder/techLogos/vscode_acuqpg.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/git_pzgqbx.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/github_k4cmtc.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607554/personal_folder/techLogos/trello_qodjqr.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607555/personal_folder/techLogos/cloudinary_vzdxg5.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607546/personal_folder/techLogos/nodemailer_f2rqxc.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/anotherMailgun_mrvki3.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696866691/personal_folder/techLogos/Axios_aulbag.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607553/personal_folder/techLogos/recaptcha_hryxei.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607547/personal_folder/techLogos/postman_fcumd7.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607545/personal_folder/techLogos/insomnia_wfbybr.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607556/personal_folder/techLogos/discord_gdfqmd.png",
+            "https://res.cloudinary.com/djngnnxvp/image/upload/v1696607551/personal_folder/techLogos/slack_gxwvjn.png"
+        ]
+    }
+}
