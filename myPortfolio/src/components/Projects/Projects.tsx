6a729c4db038402970fdd4a89086904e52410100
Nicolas <nicopua7@gmail.com>
2023-10-11 12:24:41 -0300

    Projects and Technologies components corrected

--- myPortfolio/src/components/Projects/Projects.tsx
+++ myPortfolio/src/components/Projects/Projects.tsx
@@ -1,17 +1,19 @@
-"use client";
 import Image from 'next/image';
-import data from '@/projects/projects.json';
+import data from "@/projects//userData";
 import scssProjects from '@/styles/projects/projects.module.scss';
+import Link from 'next/link';
+import { TbNetwork } from "react-icons/tb";
+import { BsGithub, BsYoutube } from "react-icons/bs";
 
 const Projects = () => {
     const { projects } = data;
-
     return (
-        <div className={scssProjects.globalCont}>
+        <div id="projects" className={scssProjects.globalCont}>
             <h1>Proyectos</h1>
             <div className={scssProjects.contAllCards}>
-                {projects.reverse().map((project, index : number)=>{
+                {projects.map((project, index : number)=>{
                     return (<>
+                        <Link href={`/project/${project.id}`}>
                         <div key={`${index}`} className={scssProjects.contCard}>
                             <div className={scssProjects.contImg}>
                                 <Image className={scssProjects.projectImg} width={1280} height={720} src={project.images[0]} alt='photo'/>
@@ -20,18 +22,35 @@ const Projects = () => {
                                 <h3>{project.name}</h3>
                                 <p>{project.description}</p>
                                 <div className={scssProjects.contTechs}>
-                                    {project.technologies.slice(0,5).map((tech)=>{
+                                    {project.technologies.slice(0,5).map((tech, index : number)=>{
                                         return(
-                                            <Image className={scssProjects.imgsTech} width={35} height={30} src={tech} alt='icon'/>
+                                            <Image key={index} className={scssProjects.imgsTech} width={35} height={30} src={tech} alt='icon'/>
                                         )
                                     })}
                                 </div>
                                 <div className={scssProjects.contButtons}>
-                                    <button>Sitio Web</button>
-                                    <button>GitHub</button>
+                                    {project.links.deploy? 
+                                        <Link className={scssProjects.contIcons} href={project.links.deploy} target="_blank" rel="noopener noreferrer">
+                                            <TbNetwork />
+                                        </Link>
+                                    : <></>
+                                    }
+                                    {project.links.github? 
+                                        <Link className={scssProjects.contIcons} href={project.links.github} target="_blank" rel="noopener noreferrer">
+                                            <BsGithub />
+                                        </Link>
+                                    : <></>
+                                    }
+                                    {project.links.youtube? 
+                                        <Link className={scssProjects.contIcons} href={project.links.youtube} target="_blank" rel="noopener noreferrer">
+                                            <BsYoutube />
+                                        </Link>
+                                    : <></>
+                                    }
                                 </div>
                             </div>
                         </div>
+                        </Link>
                     </>)
                 })}
             </div>
--- myPortfolio/src/components/Technologies/Technologies.tsx
+++ myPortfolio/src/components/Technologies/Technologies.tsx
@@ -1,7 +1,52 @@
+import Image from "next/image";
+import data from "@/projects/userData"
+import techss from "@/styles/technologies/techs.module.scss"
+
 const Technologies = () => {
+    const { technologies: {database , desing, techs, tools} } = data; 
     return(
-        <div>
-            
+        <div id="techs" className={techss.globalCont}>
+            <h1>Tecnologías & Herramientas</h1>
+            <h2>Tecnologías</h2>
+            <div className={techss.contAllCards}>
+                {techs.map((img : string, index: number)=>{
+                    return (<>
+                        <div className={techss.contCard} key={`${index}`}>
+                            <Image width={100} height={100} src={img} alt="techs"/>
+                        </div>
+                    </>)
+                })}
+            </div>
+            <h2>Diseño</h2>
+            <div className={techss.contAllCards}>
+                {desing.map((img : string, index: number)=> {
+                    return (<>
+                        <div className={techss.contCard} key={`${index}`}>
+                            <Image width={100} height={100} src={img} alt="dbimgs"/>
+                        </div>
+                    </>)
+                })}
+            </div>
+            <h2>Base de datos</h2>
+            <div className={techss.contAllCards}>
+                {database.map((img : string, index: number)=> {
+                    return (<>
+                        <div className={techss.contCard} key={`${index}`}>
+                            <Image width={100} height={100} src={img} alt="dbimgs"/>
+                        </div>
+                    </>)
+                })}
+            </div>
+            <h2>Herramientas</h2>
+            <div className={techss.contAllCards}>
+                {tools.map((img : string, index: number)=> {
+                    return (<>
+                        <div className={techss.contCard} key={`${index}`}>
+                            <Image width={100} height={100} src={img} alt="dbimgs"/>
+                        </div>
+                    </>)
+                })}
+            </div>
         </div>
     )
 }
